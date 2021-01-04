8310c83e6f8602315e8770eac591da93823dc4cf
Blas Rodriguez Irizar <rodrigblas@gmail.com>
2021-01-05 00:19:20 +0100

    lib: wrap Ring in Arc

--- src/lib.rs
+++ src/lib.rs
@@ -11,24 +11,42 @@ pub mod reader;
 pub mod ring;
 pub mod writer;
 
+use reader::Reader;
 use ring::Ring;
 
 use anyhow::Result;
+
+use std::sync::Arc;
 pub struct Bondi<T> {
-    ring: Ring<T>,
+    ring: Arc<Ring<T>>,
 }
 
 impl<T: Clone> Bondi<T> {
     pub fn new(capacity: usize) -> Self {
         Self {
-            ring: Ring::new(capacity),
+            ring: Arc::new(Ring::new(capacity)),
         }
     }
     pub fn get_tx(&self) -> Result<()> {
         Ok(())
     }
 
-    pub fn get_rx(&self) -> Result<()> {
-        Ok(())
+    pub fn get_rx(&self) -> Result<Reader<T>> {
+        Ok(Reader::new(Arc::clone(&self.ring)))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn get_a_reader() {
+        let bondi = Bondi::<usize>::new(100);
+        let mut reader = bondi.get_rx().unwrap();
+        std::thread::spawn(move || {
+            let _ = reader.read();
+        });
+        // there is nothing to read, so don't join
+        std::thread::sleep(std::time::Duration::from_millis(100));
     }
 }
